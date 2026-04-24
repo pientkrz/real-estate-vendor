d090c396a7823236585bd1c93754a5c47b1601f6
pientkrz <pientka.k@gmail.com>
2026-04-24 21:31:00 +0200

    chore: configure github pages deployment with subpath /real-estate-vendor/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pientkrz.github.io',
+  base: '/real-estate-vendor/',
   integrations: [react()],
   output: 'static',
 });
